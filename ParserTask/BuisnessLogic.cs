20ac3a3dc13f7fca3e5fa2cc786b6be47f6c55fe
OnlyM1ss <45197509+OnlyM1ss@users.noreply.github.com>
2020-08-15 18:57:27 +0400

    Add files via upload

---
+++ ParserTask/BuisnessLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParserTask
+{
+    //make all buisness logic 
+    class BuisnessLogic
+    {
+        string url = @"https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=beautiful&site=stackoverflow";
+        private void getJson()
+        {
+            ParseData parseData = new ParseData();
+            parseData.DownloadFile(url);
+            parseData.Decompress("json.gz", "json.json");
+            parseData.readJson("json.json");
+        }
+        public BuisnessLogic()
+        {
+            getJson();
+        }
+    }
+}
